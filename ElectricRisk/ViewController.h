60fd6b992a7396f71665821ae4f89dcbc6fba981
Yachen Dai <yasinzhang258@gmail.com>
2016-08-26 23:02:43 +0800

    Initial Commit

---
+++ ElectricRisk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ElectricRisk
+//
+//  Created by Yachen Dai on 8/26/16.
+//  Copyright © 2016 com.yasin.electric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ElectricRisk/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ElectricRisk
+//
+//  Created by Yachen Dai on 8/26/16.
+//  Copyright © 2016 com.yasin.electric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
